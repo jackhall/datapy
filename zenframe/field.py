61781129366ae944b4c326772d19853de56ef2f9
Jack Hall <jackwhall7@gmail.com>
2020-05-25 01:43:08 -0500

    eliminate protocol module, refactor index and field modules

--- zenframe/field.py
+++ zenframe/field.py
@@ -5,61 +5,97 @@ import attr
 import numpy as np
 
 from .common import delegate
-from .protocols import (
+from .index import (
     ComposeableIndex,
-    IndexedNullableField,
 )
 
 
 T = ty.TypeVar('T')
 U = ty.TypeVar('U')
+FROM_IDX = ty.TypeVar('FROM_IDX')
+
+
+class IndexedNullableField(ty.Collection[T], ty.Protocol[FROM_IDX]):
+    index: ComposeableIndex
+
+    def __getitem__(self, idx: FROM_IDX) -> T:
+        pass
+
+    def __setitem__(self, idx: FROM_IDX, value: T) -> None:
+        """ if idx exists, replace the value; if not, raise an exception """
+        pass
+
+    def map(self, func: ty.Callable[[T], ty.Any]) -> 'IndexedNullableField':
+        """ apply `func` to every item """
+        pass
+
+    def filter(self, pred: ty.Callable[[T], bool]) -> 'IndexedNullableField':
+        """ unindex each element for which `pred` is False (in new Series) """
+        pass
+
+    def accum(self, binary_func: ty.Callable[[U, T], U]) -> U:
+        """ a more generic form of reduce """
+        pass
+
+    def resolve(self) -> 'IndexedNullableField':
+        """ copy array and flatten index """
+        ...
+
+
+@attr.s(auto_attribs=True)
+class NullableField(ty.Generic[T]):
+    _array: np.ndarray = attr.ib()
+    _null_mask: np.ndarray = attr.ib()  # where mask is False, elements are null
+
+    def __getitem__(self, idx) -> ty.Optional[T]:
+        return self._array[idx] if self._null_mask[idx] else None
+
+    def __setitem__(self, idx, value: ty.Optional[T]) -> None:
+        """ if idx exists, replace the value; if not, raise an exception """
+        if value is None:
+            self._null_mask[idx] = False
+        else:
+            self._array[idx] = value
+
+    def __contains__(self, value: T) -> bool:
+        indices = np.where(self._array == value)[0]
+        return any(self._null_mask[idx] for idx in indices)
+
+    def map(self, func: ty.Callable[[T], ty.Any]) -> IndexedNullableField:
+        """ apply `func` to every item """
+        new_array = copy.copy(self._array)
+        for idx, value in np.ndenumerate(self._array):
+            if self._null_mask[idx]:
+                new_array[idx] = func(value)
+        return attr.evolve(self, array=new_array)
+
+    def accum(self, binary_func: ty.Callable[[U, T], U], initializer: U) -> U:
+        ...
 
 
 @attr.s(auto_attribs=True)
 class Field(ty.Generic[T]):
     """ sequence of T """
-    _array: np.ndarray = attr.ib()
-    _null_mask: np.ndarray = attr.ib()  # each element is False if None
+    _field: NullableField[T]
     index: ComposeableIndex
 
     def __getitem__(self, idx) -> ty.Optional[T]:
-        np_idx = self.index.find(idx)
-        if self._null_mask[np_idx]:
-            return self._array[np_idx]
-        else:
-            return None
+        return self._field[self.index.find(idx)]
 
-    def __setitem__(self, idx, value: ty.Optional[T]):
-        """ if idx exists, replace the value; if not, raise an exception """
-        np_idx = self.index.find(idx)
-        if value is None:
-            self._null_mask[np_idx] = False
-        else:
-            self._array[np_idx] = value
+    def __setitem__(self, idx, value: ty.Optional[T]) -> None:
+        self._field[self.index.find(idx)] = value
 
-    def __iter__(self):
+    def __iter__(self) -> ty.Iterator[T]:
         for idx in self.index:
             yield self[idx]
 
-    def __contains__(self, value: T):
-        indices = np.where(self._array == value)[0]
-        return any(self._null_mask[idx] for idx in indices)
-
+    __contains__ = delegate('__contains__', '_field')
     __len__ = delegate('__len__', 'index')
-
-    def map(self, func: ty.Callable[[T], ty.Any]) -> IndexedNullableField:
-        """ apply `func` to every item """
-        new_array = copy.copy(self._array)
-        for np_idx, value in np.ndenumerate(self._array):
-            if self._null_mask[np_idx]:
-                new_array[np_idx] = func(value)
-        return attr.evolve(self, array=new_array)
+    map = delegate('map', '_field')  # won't necessarily iterate in the index order
+    accum = delegate('accum', '_field')  # won't necessarily iterate in the index order
 
     def filter(self, pred: ty.Callable[[T], bool]) -> IndexedNullableField:
         """ unindex each element for which `pred` is False (in new Series) """
-        ...
-
-    def accum(self, binary_func: ty.Callable[[U, T], U]) -> U:
         ...
 
     def resolve(self) -> IndexedNullableField:
--- zenframe/index.py
+++ zenframe/index.py
@@ -1,14 +1,57 @@
+import copy
 import functools as ft
+import itertools as it
+import operator as op
 import typing as ty
 
 import attr
 
 from .common import delegate
-from .protocols import (
-    ComposeableIndex,
-    FROM_IDX,
-    TO_IDX,
-)
+
+
+FROM_IDX = ty.TypeVar('FROM_IDX')
+TO_IDX = ty.TypeVar('TO_IDX')
+
+
+class ComposeableIndex(ty.Collection[FROM_IDX], ty.Protocol[TO_IDX]):
+    """ Encapsulates a mapping from user-specified index values to indicies
+    to a np.ndarray: the `find` method. This mapping can be right-composed with
+    others to create new indexes. The more convoluted the composition, the
+    more expensive it will become to access the underlying numpy arrays, until
+    the user decides to reshape.
+
+    This index protocol can't handle slices yet, but eventually it should.
+    For this reason, an index cannot be a `ty.Mapping`.
+    """
+    def find(self, idx: FROM_IDX) -> TO_IDX:
+        """ Returns whatever is needed to index a numpy array. """
+        raise NotImplementedError
+
+    def fits_around(self, inner: 'ComposeableIndex') -> bool:
+        """ True if this index can be composed with `inner` safely. """
+        return all(self.find(idx) in inner for idx in self)
+
+    def compose(self, inner: 'ComposeableIndex', verify: bool = False) -> 'ComposeableIndex':
+        if verify and not self.fits_around(inner):
+            raise IndexError('the domain of inner does not match the codomain of self')
+
+        new_index = copy.copy(self)
+
+        @ft.wraps(new_index.find)
+        def composed(idx):
+            return inner.find(self.find(idx))
+
+        new_index.find = composed
+        return new_index
+
+    def items(self) -> ty.Iterable[ty.Tuple[FROM_IDX, TO_IDX]]:
+        return zip(self, map(self.find, self))
+
+    def __eq__(self, other: 'ComposeableIndex') -> bool:
+        return all(x == y for x, y in it.zip_longest(self.items(), other.items()))
+
+    def __hash__(self) -> int:
+        return ft.reduce(op.xor, map(hash, self.items()))
 
 
 def _reraise_index_error(*errors, find_method=None):
@@ -36,19 +79,19 @@ def _check_contains_first(find_method):
     return new_find_method
 
 
-# could a variation of this be used for sorting?
 @attr.s(auto_attribs=True, slots=True, frozen=True)
-class SequenceIndex(ComposeableIndex[FROM_IDX, int]):
-    _idx_seq: ty.Sequence[FROM_IDX] = attr.ib()
+class SequenceIndex(ComposeableIndex[int, TO_IDX], ty.Generic[TO_IDX]):
+    """ Useful for sorting when right-composed with an existing index (so TO_IDX==int). """
+    idx_seq: ty.Sequence[TO_IDX] = attr.ib()
 
-    __contains__ = delegate('__contains__', '_idx_seq')
-    __len__ = delegate('__len__', '_idx_seq')
-    find = _reraise_index_error(ValueError, find_method=delegate('index', '_idx_seq'))
+    __contains__ = delegate('__contains__', 'idx_seq')
+    __len__ = delegate('__len__', 'idx_seq')
+    find = _reraise_index_error(IndexError, find_method=delegate('__getitem__', 'idx_seq'))
 
     def __iter__(self):
-        return iter(sorted(self._idx_seq))
+        return range(len(self.idx_seq))
 
-    @_idx_seq.validator
+    @idx_seq.validator
     def check_unique(self, attribute, value):
         if len(value) > len(set(value)):
             raise ValueError('sequence elements must be unique')
@@ -56,20 +99,20 @@ class SequenceIndex(ComposeableIndex[FROM_IDX, int]):
 
 @attr.s(auto_attribs=True, slots=True, frozen=True)
 class DictIndex(ComposeableIndex[FROM_IDX, TO_IDX]):
-    _idx_mapping: ty.Dict[TO_IDX, FROM_IDX]
+    mapping: ty.Mapping[TO_IDX, FROM_IDX]
 
-    __contains__ = delegate('__contains__', '_idx_mapping')
-    __iter__ = delegate('__iter__', '_idx_mapping')
-    __len__ = delegate('__len__', '_idx_mapping')
-    find = _reraise_index_error(KeyError, find_method=delegate('__getitem__', '_idx_mapping'))
+    __contains__ = delegate('__contains__', 'mapping')
+    __iter__ = delegate('__iter__', 'mapping')
+    __len__ = delegate('__len__', 'mapping')
+    find = _reraise_index_error(KeyError, find_method=delegate('__getitem__', 'mapping'))
 
 
 @attr.s(auto_attribs=True, slots=True, frozen=True)
 class FunctionIndex(ComposeableIndex[FROM_IDX, TO_IDX]):
-    _func: ty.Callable[[FROM_IDX], TO_IDX]
-    _domain: ty.Collection[FROM_IDX]
+    func: ty.Callable[[FROM_IDX], TO_IDX]
+    domain: ty.Collection[FROM_IDX]
 
-    __contains__ = delegate('__contains__', '_domain')
-    __iter__ = delegate('__iter__', '_domain')
-    __len__ = delegate('__len__', '_domain')
-    find = _check_contains_first(delegate('__call__', '_func'))
+    __contains__ = delegate('__contains__', 'domain')
+    __iter__ = delegate('__iter__', 'domain')
+    __len__ = delegate('__len__', 'domain')
+    find = _check_contains_first(delegate('__call__', 'func'))
